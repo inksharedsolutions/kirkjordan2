3d5be27f282305dc2fe24917935f7fe0af97bdac
inksharedsolutions <webdev.inkshared@gmail.com>
2022-01-31 10:30:27 +0800

    QA changes

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -43,11 +43,9 @@ const ATB = (props) => {
                             </div>
                         </div>
 
-                        <article
-                            className="article-section" id="author">
-                            {/* <Slider {...settings}> */}
+                        <article className="article-section" id="author">
                             <div className="article-flex">
-                                <div className="article-flex-item">
+                                <div className="article-flex-item-left">
                                     <p>
                                         Kirk Frank Jordan was born in Greenville Pennsylvania April 11, 1965.  He graduated from Greenville High School in 1983, Fort Lewis College in Colorado in 1988.  From there he traveled to Australia for 2 years to help a couple friends develop some business ideas.  His return to America landed him in Boston Massachusetts installing equipment in a manufacturing facility for a season.  He then joined forces with B.F. Goodrich traveling the country from Florida, Wisconsin, to the Baja California for the Baja 1000.
                                     </p>
@@ -55,15 +53,27 @@ const ATB = (props) => {
                                     <p>
                                         At the end of that contract he rested at his fathers’ home in Hadley Pennsylvania for the holidays and then set sail in the spring for Pat Wolfe Log Building School in Ontario Canada.  Back to Hadley to regroup and head south to Mississippi to spend the winter as a “reforestation technician”, a fancy term for a “pine setter”.  Back to Hadley for some r and r and then west to Missoula Montana where he would be employed by Custom Log Homes for a spell.
                                     </p>
-                                </div>
-                                <div className="article-flex-item">
                                     <p>
                                         Pat Wolfe’s log school carried some weight and Kirk soon found himself in Maupin Oregon working for Oregon Log Homes.  Oregon Log Homes landed a big contract with Disneyland in Florida to do the log work for “Fort Wilderness Lodge”.  Once that contract was complete, he returned to Pennsylvania to re-tool and head to Alaska where he was petitioned to build a cabin north of the Arctic Circle in the wilderness on the edge of Lake Chandlar.  Working on the Kenai Peninsula and Juneau, he would return to Hadley to catch his breath before heading to Utah where his chainsaw was requested once again.
                                     </p>
 
+                                </div>
+                                <div className="article-flex-item-right">
                                     <p>
                                         In 1996 he married a kindergarten teacher while simultaneously buying a home and property and then heading to Delaware to build another log home.  One more log home in Illinois was enough experience to settle him down in Hadley.
                                         From there he would start “Jordan Log Homes” and build log homes and cabins until God would assign him to be fishers of men.   As a good gesture, not recognizing his need for a Savior, Kirk would attend the church he was raised in as a youngster, First Presbyterian Church Greenville Pa.
+                                    </p>
+                                    <p>
+                                        After 4 Sundays there the pastor was called to Michigan and Kirk was invited to Hillside Presbyterian Church by friends.  There he would become a member and elder until his call to Hadley Presbyterian Church where he would be
+                                        mentored by the Rev. Dr. VanDyne.  “Chuck” recognizing the divinity in the appointment would put Kirk in the game, asking him to preach on occasion and then regularly during the Sunrise Service for Easter.  Compelled by Chucks
+                                        passing on December 25, 2009 Kirk would enroll in classes at the Dubuque Theological Seminary.  Also attending classes at Thiel College and Pittsburgh Theological Seminary Kirk would continue to build while simultaneously “fill
+                                        in” in pulpits around the area.
+                                    </p>
+                                    <p>
+                                        On such an occasion he would find himself at North Sandy Presbyterian Church as a “guest” speaker.  Therein would the next invitation come to fill their pulpit and by God’s grace he would remain there
+                                        until 2020.  Of that year, on “Good Friday” God’s spirit would compel Kirk too fast for the first time in his life and on his birthday would begin writing his first book. In August of 2019 Kirk would travel to Alaska’s Chichagof Island
+                                        to preach the gospel in the little village of Hoonah. Transformed by the Alaska trip and upon his return to North Sandy at the church’s monthly meeting in October providence would bid Kirk to ask the elders to begin the search for his replacement.
+                                        Today, from his country home in northwestern Pennsylvania Kirk continues to prepare weekly sermons, all the while working on his next book.  Rejoice always, pray without ceasing, and in everything, give thanks.
                                     </p>
                                 </div>
                             </div>
@@ -74,7 +84,42 @@ const ATB = (props) => {
                                     Kirk Jordan
                                 </span>
                             </p>
+                        </article>
 
+                        <article  className="article-section-android">
+                            <p>
+                                Kirk Frank Jordan was born in Greenville Pennsylvania April 11, 1965.  He graduated from Greenville High School in 1983, Fort Lewis College in Colorado in 1988.  From there he traveled to Australia for 2 years to help a couple friends develop some business ideas.  His return to America landed him in Boston Massachusetts installing equipment in a manufacturing facility for a season.  He then joined forces with B.F. Goodrich traveling the country from Florida, Wisconsin, to the Baja California for the Baja 1000.
+                            </p>
+
+                            <p>
+                                At the end of that contract he rested at his fathers’ home in Hadley Pennsylvania for the holidays and then set sail in the spring for Pat Wolfe Log Building School in Ontario Canada.  Back to Hadley to regroup and head south to Mississippi to spend the winter as a “reforestation technician”, a fancy term for a “pine setter”.  Back to Hadley for some r and r and then west to Missoula Montana where he would be employed by Custom Log Homes for a spell.
+                            </p>
+                            <p>
+                                Pat Wolfe’s log school carried some weight and Kirk soon found himself in Maupin Oregon working for Oregon Log Homes.  Oregon Log Homes landed a big contract with Disneyland in Florida to do the log work for “Fort Wilderness Lodge”.  Once that contract was complete, he returned to Pennsylvania to re-tool and head to Alaska where he was petitioned to build a cabin north of the Arctic Circle in the wilderness on the edge of Lake Chandlar.  Working on the Kenai Peninsula and Juneau, he would return to Hadley to catch his breath before heading to Utah where his chainsaw was requested once again.
+                            </p>
+                            <p>
+                                In 1996 he married a kindergarten teacher while simultaneously buying a home and property and then heading to Delaware to build another log home.  One more log home in Illinois was enough experience to settle him down in Hadley.
+                                From there he would start “Jordan Log Homes” and build log homes and cabins until God would assign him to be fishers of men.   As a good gesture, not recognizing his need for a Savior, Kirk would attend the church he was raised in as a youngster, First Presbyterian Church Greenville Pa.
+                            </p>
+                            <p>
+                                After 4 Sundays there the pastor was called to Michigan and Kirk was invited to Hillside Presbyterian Church by friends.  There he would become a member and elder until his call to Hadley Presbyterian Church where he would be
+                                mentored by the Rev. Dr. VanDyne.  “Chuck” recognizing the divinity in the appointment would put Kirk in the game, asking him to preach on occasion and then regularly during the Sunrise Service for Easter.  Compelled by Chucks
+                                passing on December 25, 2009 Kirk would enroll in classes at the Dubuque Theological Seminary.  Also attending classes at Thiel College and Pittsburgh Theological Seminary Kirk would continue to build while simultaneously “fill
+                                in” in pulpits around the area.
+                            </p>
+                            <p>
+                                On such an occasion he would find himself at North Sandy Presbyterian Church as a “guest” speaker.  Therein would the next invitation come to fill their pulpit and by God’s grace he would remain there
+                                until 2020.  Of that year, on “Good Friday” God’s spirit would compel Kirk too fast for the first time in his life and on his birthday would begin writing his first book. In August of 2019 Kirk would travel to Alaska’s Chichagof Island
+                                to preach the gospel in the little village of Hoonah. Transformed by the Alaska trip and upon his return to North Sandy at the church’s monthly meeting in October providence would bid Kirk to ask the elders to begin the search for his replacement.
+                                Today, from his country home in northwestern Pennsylvania Kirk continues to prepare weekly sermons, all the while working on his next book.  Rejoice always, pray without ceasing, and in everything, give thanks.
+                            </p>
+
+                            <p>
+                                <span className="author-name">
+                                    <span className="author-span-ft">Author |  Writer </span>
+                                    Kirk Jordan
+                                </span>
+                            </p>
                         </article>
 
 
--- src/pages/about-the-book.js
+++ src/pages/about-the-book.js
@@ -7,63 +7,63 @@ import { DiscussionEmbed } from "disqus-react"
 import Book1 from '../../static/book/book.png'
 import { Helmet } from "react-helmet"
 
-const ATB = (props)=>{
+const ATB = (props) => {
 
 
-    const slug =(props.location.href);
-    
-	const disqusConfig = {
-	  shortname: 'kirk-jordon',
-	  config: { identifier:  12, slug}
+    const slug = (props.location.href);
+
+    const disqusConfig = {
+        shortname: 'kirk-jordon',
+        config: { identifier: 12, slug }
     }
-    
 
-	return (
-		<>
-		 	<Layout>
-             <Helmet title="About the Book | Kirk Jordan"/>
-		 		<Nav pathExt={props.path}/>
-            
-		 		<Banner
+
+    return (
+        <>
+            <Layout>
+                <Helmet title="About the Book | Kirk Jordan" />
+                <Nav pathExt={props.path} />
+
+                <Banner
                     spanFirst={`About The`}
                     contextHeading={`Book`}
-                 />
+                />
 
                 <main className="wrapperMain">
-                    <div 
+                    <div
                         className="container"
                         id="book-containers" >
 
-                            <BookInfo
-                                data={{
-                                    title:`The Best Thing`,
-                                    spanTitle: 'My Personal Journey to Know God',
-                                    imgSrc: Book1,
-                                    id:'the_gifted_one',
-                                    content:
-                                    `x
-                                    The Best Thing is partly my story but mostly about God's amazing grace. I've been reading and writing my whole life, and providence has compelled me far too long to write a book, so here we are. The book may be crude and raw, but outside Calvary's forgiveness, it's neither worth writing or reading.
+                        <BookInfo
+                            data={{
+                                title: `The Best Thing`,
+                                spanTitle: 'My Personal Journey to Know God',
+                                imgSrc: Book1,
+                                id: 'the_gifted_one',
+                                content:
+                                    `
+                                    The Best Thing is partly my story but mostly about God's amazing grace. I've been reading and writing my whole life, and providence has compelled me far too long to write a book, so here we are. The book may be crude and raw, but outside Calvary's forgiveness, it's neither worth writing or reading. I honestly feel like the "least of all of these," not really gifted in any particular way, willing, though not particularly gifted. If I have a gift, it is in believing in the One that saved me, obedience to the One that called me. The Best Thing is meant to be driven, to stir you that you might exit the book transformed from your entrance. Don't leave the way you came. The book is designed to make you mad or get you excited, move you to sing your song, understand you are gifted, and share those gifts in a way that will make the world a better place. When I started the book, friends and family asked, "What is the book about?" God's amazing grace. It had to be His idea. Otherwise, it's just another book sitting on the shelf. It's in His hands. It is for His purpose and His glory. At the end of the day, when we do the math, consider the earth's spinning, rotating 23.5-degree axis, there is no other reasonable explanation for my testimony, my story, or yours save God's amazing grace. This book is for all people everywhere. It covers the globe, all races, religions, colors, and geographical locations. There are people everywhere living in the bondage of thousands of years of unforgiveness. God nailed His Son to a cross that we might be forgiven and forgive one another. This book is a call to put down our arms, receive God's payment on Calvary's shore, take up our crosses, and love one another. Jesus is not a thing. In fact, He is many things, so we'll just leave you with The Best Thing and pray that you drive it and hear the call within its pages to sing your song.
                                     `,
-                                    ebooks:{
-                                        stratton : '',
-                                        barnes: 'https://www.barnesandnoble.com/w/the-best-thing-kirk-jordan/1139976648?ean=9781648954870',
-                                        amazon :'https://www.amazon.ca/Best-Thing-Kirk-Jordan-ebook/dp/B09C22HG3D/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1628695054&sr=1-1', 
-                                    },
-                                    paperback:{
-                                        amazon:'https://www.amazon.ca/Best-Thing-Kirk-Jordan/dp/1648954863/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1628695054&sr=1-1',
-                                        barnes:'https://www.barnesandnoble.com/w/the-best-thing-kirk-jordan/1139976648?ean=9781648954863',
-                                        booksamillion:'https://www.booksamillion.com/p/Best-Thing/Kirk-Jordan/9781648954863?id=8158356429268',
-                                    },
-                                }}
-                            />
-                            <div className="commentSection" >
-                                <DiscussionEmbed {...disqusConfig} />
-                            </div>
-                    </div>	
+                                ebooks: {
+                                    stratton: '',
+                                    barnes: 'https://www.barnesandnoble.com/w/the-best-thing-kirk-jordan/1139976648?ean=9781648954870',
+                                    amazon: 'https://www.amazon.ca/Best-Thing-Kirk-Jordan-ebook/dp/B09C22HG3D/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1628695054&sr=1-1',
+                                },
+                                paperback: {
+                                    amazon: 'https://www.amazon.ca/Best-Thing-Kirk-Jordan/dp/1648954863/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1628695054&sr=1-1',
+                                    barnes: 'https://www.barnesandnoble.com/w/the-best-thing-kirk-jordan/1139976648?ean=9781648954863',
+                                    booksamillion: 'https://www.booksamillion.com/p/Best-Thing/Kirk-Jordan/9781648954863?id=8158356429268',
+                                },
+                            }}
+                        />
+                        <div className="commentSection" >
+                            <DiscussionEmbed {...disqusConfig} />
+                        </div>
+                    </div>
                 </main>
-		 	</Layout>
-		</>
-	)
+            </Layout>
+        </>
+    )
 }
 
 
